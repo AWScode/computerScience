858ff0c2d3355a942b65f2a271f9c69e203e223b
Shubing Sun <shubing_sun@m013460.aw.org>
2017-09-13 13:43:01 -0700

    Trump sucks

---
+++ coffeeShop/coffeeShop.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.until.Scanner;
+
+public class coffeeShop {
+
+	public static void main(String[] args) {
+
+		System.out.println("Welcome to Deja Brew, may I take your order?");
+		Scanner order = new Scanner(System.in);
+
+    }
+
+
+  }
