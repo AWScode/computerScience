fc2eb07df82b4f29a42ad6ff88ecfa96b2c8a393
Shubing Sun <shubing_sun@m013460.aw.org>
2018-02-02 13:45:34 -0800

    yeet

--- Prime.java
+++ Prime.java
@@ -2,19 +2,20 @@ import java.util.*;
 import java.math.*;
 
 public class Prime {
-  public static boolean isPrime(long number) {
-    for (long a = 2; a < number; a++) {
+  public static boolean isPrime(int number) {
+    for (int a = 2; a <= number/2; a++) {
         if (number % a == 0) {
             return false;
         }
         else{
-          break;
+          return true;
         }
       }
-    return true;}
+    return true;
+    }
 
-  public static long nextPrime(long x){
-    long j = x + 1;
+  public static int nextPrime(int x){
+    int j = x + 1;
     while(true){
       if(isPrime(x)){
         break;
@@ -23,6 +24,7 @@ public class Prime {
         j = j + 1;
       }
     }
+    System.out.println(Integer.toString(j));
     return j;
 
   }
@@ -30,9 +32,10 @@ public class Prime {
 
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
-    long i = sc.nextLong();
+    int i = sc.nextInt();
     System.out.println("Enter a number: ");
     sc.close();
+    nextPrime(i);
   }
 
 }
