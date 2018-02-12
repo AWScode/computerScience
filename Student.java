ba99b99cdd178e60de8a6b8287bbdd324efc997a
Shubing Sun <shubing_sun@m013460.aw.org>
2018-02-12 14:28:28 -0800

    ewr

---
+++ Student.java
@@ -0,0 +1,31 @@
+public class Student{
+
+  private static String name;
+  private static String class;
+  private static String aspect;
+  private static String session;
+  private static Int color;
+  private static Char dream;
+
+  public students(String n, String c, String a, String s, Int co, Char d){
+    this.name = n;
+    this.class = c;
+    this.aspect = a;
+    this.session = s;
+    this.color = co;
+    this.dream = d;
+  }
+
+  public static void dream(){
+    return this.dream;
+  }
+
+  public static void session(){
+    return this.session;
+  }
+
+
+  public static void main(String[] args) {
+
+}
+}
